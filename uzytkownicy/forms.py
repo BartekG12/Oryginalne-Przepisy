2299cd9eb11f93a692db64d531085d1685e86efe
BartekG12 <awprojektsender@gmail.com>
2022-12-11 20:08:46 +0100

    account update

--- uzytkownicy/forms.py
+++ uzytkownicy/forms.py
@@ -6,11 +6,11 @@ from .models import Profil, Message
 class TworzenieUzytkownika(UserCreationForm):
     class Meta:
         model = User
-        fields = ['first_name', 'username', 'email', 'password1', 'password2']
+        fields = ['first_name', 'email', 'username', 'password1', 'password2']
         labels = {
-            'first_name': 'pierwsze imie',
+            'first_name': 'Imię i nazwisko',
             'email': 'Adres E-mail',
-            'username': 'Imię i nazwisko',
+            'username': 'Nazwa Użytkownika',
             'password1': 'Hasło',
             'password2': 'Potwierdź hasło',
         }
