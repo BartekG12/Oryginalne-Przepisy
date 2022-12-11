8235bea101f19570ff605c42d1e67c7cb583351c
BartekG12 <awprojektsender@gmail.com>
2022-12-11 21:19:16 +0100

    account update

--- przepisyProjekt/settings.py
+++ przepisyProjekt/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4-@kz0yvs%z3x7)dz=+)8wy=9ks)57vbu2(c1sk6oz)$)ur_1-'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'przepisy.apps.PrzepisyConfig',
+    'przepisy.appqs.PrzepisyConfig',
     'uzytkownicy.apps.UzytkownicyConfig',
 ]
 
@@ -117,14 +117,12 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'stronaoryginalneprzepisy@gmail.com'
-EMAIL_HOST_PASSWORD = 'zalbwdhhkesqtkgk'
-
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
