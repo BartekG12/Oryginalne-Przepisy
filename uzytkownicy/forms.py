8ebd8e036af9e9e5156ac84a4125c0531fe10655
BartekG12 <awprojektsender@gmail.com>
2022-12-12 11:20:32 +0100

    accounts updateB

--- uzytkownicy/forms.py
+++ uzytkownicy/forms.py
@@ -1,12 +1,12 @@
 from django.forms import ModelForm
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import Profil, Message
 
 class TworzenieUzytkownika(UserCreationForm):
     class Meta:
         model = User
-        fields = ['email', 'username', 'password1', 'password2']
+        fields = ['email', 'username']
 
     def __init__(self, *args, **kwargs):
         super(TworzenieUzytkownika, self).__init__(*args, **kwargs)
