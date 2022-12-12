920f23be9fe9f8e6fc9057fb9f926344d6866c6f
BartekG12 <awprojektsender@gmail.com>
2022-12-12 10:11:45 +0100

    accounts updateB

--- przepisyProjekt/settings.py
+++ przepisyProjekt/settings.py
@@ -48,8 +48,8 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'djancsrf.middleware.csrf.CsrfViewMiddleware',
-    'djanauthen.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
