d8741b31937c8ad1e2cbe27369b47625876e839b
BartekG12 <awprojektsender@gmail.com>
2022-12-12 13:19:18 +0100

    accounts updateA

--- uzytkownicy/views.py
+++ uzytkownicy/views.py
@@ -45,6 +45,7 @@ def register(request):
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
+            user.name = user.username()
             user.save()
             messages.success(request, 'Konto zostało założone!')
             login(request, user)
