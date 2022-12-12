4e8bf779219354a0604148247fd9f4d3a945b10b
BartekG12 <awprojektsender@gmail.com>
2022-12-12 13:19:18 +0100

    accounts updateA

--- uzytkownicy/forms.py
+++ uzytkownicy/forms.py
@@ -1,18 +1,24 @@
 from django.forms import ModelForm
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from .models import Profil, Message
 
 class TworzenieUzytkownika(UserCreationForm):
     class Meta:
         model = User
-        fields = ['first_name', 'email', 'username', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2']
         labels = {
-            'first_name': 'Name',
+            'username': 'Nazwa Użytkownika',
+            'email': 'Adres E-mail',
+            'password1': 'Hasło',
+            'password2': 'Potwierdź hasło',
         }
 
     def __init__(self, *args, **kwargs):
         super(TworzenieUzytkownika, self).__init__(*args, **kwargs)
+
+        for k, v in self.Meta.labels.items():
+            self[k].label = v
 
         for name, field in self.fields.items():
             field.widget.attrs.update({'class': 'input'})
