ff941f131a03be26d74f73e0631d054c01e3c28d
BartekG12 <awprojektsender@gmail.com>
2022-12-12 12:57:29 +0100

    accounts updateB

--- przepisyProjekt/settings.py
+++ przepisyProjekt/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLST')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
